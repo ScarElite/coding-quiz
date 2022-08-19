6ec25ab8acc30182f6d19f31ce2a16067b29dbb3
Mitchell Armstrong <mra0211@gmail.com>
2022-08-18 21:06:43 -0400

    Added a create function that creates a header with the question, a ul element, and li elements that will contain the choices.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var startBtn = document.querySelector("#start-quiz");
 var mainContentEl = document.querySelector(".main-content")
 var mainHeadingEl = document.querySelector(".main-heading");
-var questions = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10"];
+var questions = ["Commonly used DataTypes DO NOT include what?", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10"];
 var choices = ["A", "B", "C", "D"];
 
 var start = function () {
@@ -9,7 +9,9 @@ var start = function () {
     mainContentEl = clear();
     console.log("Content cleared");
 
-    mainHeadingEl.textContent = questions[0];
+    mainContentEl = create();
+    console.log("Ran the create function");
+
 }
 
 var clear = function () {
@@ -17,10 +19,35 @@ var clear = function () {
 
     console.log("Removing main content");
 
-    mainHeadingEl.textContent = "";
+    mainHeadingEl.innerHTML = "";
     mainTextEl.outerHTML = "";
     // startBtn.textContent = "";
     startBtn.outerHTML = "";
+}
+
+var create = function () {
+    var newHeadingEl = mainHeadingEl;
+    // Create a for loop here
+    newHeadingEl.textContent = questions[0];
+    newHeadingEl.setAttribute("Style", "color: black; margin-bottom: 10%;");
+
+    var arr = ["A:", "B:", "C:", "D:"];
+    var cont = document.getElementById('container');
+
+    // create ul element and set the attributes.
+    var choiceList = document.createElement('ul');
+    choiceList.className = "choiceList";
+    choiceList.setAttribute('id', 'theList');
+
+    for (i = 0; i <= arr.length - 1; i++) {
+        var choice = document.createElement('li');
+        choice.innerHTML = arr[i];
+        choice.className = "choice";
+
+        choiceList.appendChild(choice);
+    }
+
+    cont.appendChild(choiceList);
 }
 
 
