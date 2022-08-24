a9ccd6eaa9c3381ac20d17631888d5a9c74fc141
Mitchell Armstrong <mra0211@gmail.com>
2022-08-23 20:27:14 -0400

    Added a displayQuestion function to create the question and also randomized the order the questions appear when the user begins the quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,115 @@
 var startBtn = document.querySelector("#start-quiz");
+// Variables for the timer
 var timer = document.querySelector(".time");
-var count = 100;
-var mainContentEl = document.querySelector(".main-content")
+var timerStart = 100;
+// Main content variables
+var mainContentEl = document.querySelector(".main-content");
 var mainHeadingEl = document.querySelector(".main-heading");
-var questions = ["Commonly used data types DO NOT include what?", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10"];
+// Variables for the Quiz questions and answers
 var choices = ["A", "B", "C", "D"];
-var questions = [
+var arrayShuffledQuestions;
+var currentQuestionIndex = 0;
+const questions = [
     {
         question: "Commonly used data types DO NOT include what?",
-        choices: ["Test 1", "Test 2", "Test 3", "Test 4"],
-        correctAnswer: "Test 3"
-    }
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What does 'DOM' stand for?",
+        choices: {
+            a: "Documentation Of Methods",
+            b: "Don't Overlook Methods",
+            c: "Document Oriented Model",
+            d: "Document Object Model"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Test Question 3",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Test Question 4",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Test Question 5",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Test Question 6",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Test Question 7",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Test Question 8",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Test Question 9",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Test Question 10",
+        choices: {
+            a: "Test 1",
+            b: "Test 2",
+            c: "Test 3",
+            d: "Test 4"
+        },
+        correctAnswer: "c"
+    },
 ]
 
 var start = function () {
@@ -18,11 +117,35 @@ var start = function () {
     mainContentEl = clearMain();
     console.log("Content cleared");
 
-    mainContentEl = create();
-    console.log("Ran the create function");
-
     runTimer();
+
+    // Randomize the order that the questions appear
+    arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    setQuestion();
+    create();
 }
+
+var setQuestion = function () {
+    displayQuestion(arrayShuffledQuestions[currentQuestionIndex]);
+}
+
+// var resetChoices = function() {
+//     while ()
+// }
+
+var displayQuestion = function (index) {
+    mainHeadingEl.innerText = index.question;
+    mainHeadingEl.classList.add("main-heading-question");
+
+    // for (var i = 0; i < index.choices.length; i++) {
+    //     var answerBtn = document.createElement("button");
+    //     answerBtn.innerText = index.choices[i].choice;
+    //     answerBtn.classList.add("btn");
+    //     answerBtn.classList.add("choice");
+    //     answerBtn.addEventListener("click", answerCheck);
+    //     mainHeadingEl.appendChild(answerBtn);
+    // }
+};
 
 var clearMain = function () {
     var mainTextEl = document.querySelector("#main-text");
@@ -36,11 +159,6 @@ var clearMain = function () {
 }
 
 var create = function () {
-    var newHeadingEl = mainHeadingEl;
-    // Create a for loop here
-    newHeadingEl.textContent = questions[0];
-    newHeadingEl.setAttribute("Style", "color: black; margin-bottom: 5%; text-align: left");
-
     var arr = ["A:", "B:", "C:", "D:"];
     var cont = document.getElementById('container');
 
@@ -53,16 +171,13 @@ var create = function () {
     }
 }
 
-var clearQuestion = function () {
-    newHeadingEl.textContent = "";
-}
-
 function runTimer() {
-    var time = setInterval(function () {
-        timer.innerHTML = count;
-        if (count > 0) {
-            count--;
+    setInterval(function () {
+        timer.innerHTML = timerStart;
+        if (timerStart > 0) {
+            timerStart--;
         }
+        // else if timerStart = 0 then quiz is over
     }, 1000);
 };
 
